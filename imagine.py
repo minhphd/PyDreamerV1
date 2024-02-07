3e562a98a0f1709f45bb5bf793584861fceb3f89
Minh Pham Dinh <mhpham26@colby.edu>
2024-02-07 14:24:44 -0500

    added action repeat

---
+++ imagine.py
@@ -0,0 +1,75 @@
+import sys
+import argparse
+from utils.wrappers import DMCtoGymWrapper, AtariPreprocess
+from addict import Dict
+import yaml
+import gymnasium as gym
+import torch
+from tqdm import tqdm
+import numpy as np
+import glob
+
+parser = argparse.ArgumentParser(description='Process configuration file path.')
+parser.add_argument('--runpath', type=str, help='Path to the run file.', required=True)
+parser.add_argument('--horizon', type=int, help='number of imagination steps.', default=15)
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Load the configuration file specified by the command line argument
+run_path = args.runpath
+HORIZON = args.horizon
+
+config_files = glob.glob(run_path + '/config/*.yml')
+
+if len(config_files) != 1:
+    print('there should only be 1 config file in config directory')
+
+with open(config_files[0], 'r') as file:
+    config = Dict(yaml.load(file, Loader=yaml.FullLoader))
+
+env_id = config.env.env_id
+
+if 'ALE' in config.env.env_id:
+    env = gym.make(env_id, render_mode='rgb_array')
+    env = AtariPreprocess(env, config.env.new_obs_size, 
+                          False)
+else:
+    task = config.env.task
+    env = DMCtoGymWrapper(env_id, task,
+                          resize=config.env.new_obs_size,
+                          record=False)
+
+print("start imagining")
+
+encode = torch.load(run_path + '/models/encoder', map_location=torch.device('cpu') )
+decoder = torch.load(run_path + '/models/decoder', map_location=torch.device('cpu') )
+rssm = torch.load(run_path + '/models/rssm_model', map_location=torch.device('cpu') )
+actor = torch.load(run_path + '/models/actor', map_location=torch.device('cpu'))
+
+obs, _ = env.reset()
+
+posterior = torch.zeros((1, config.main.stochastic_size))
+deterministic = torch.zeros((1, config.main.deterministic_size))
+e_obs = encode(torch.from_numpy(obs).to(dtype=torch.float))
+
+_, posterior = rssm.representation(e_obs, deterministic)
+    
+from PIL import Image
+
+frames = []
+
+for i in tqdm(range(200)):
+    actions = actor(posterior, deterministic)
+    deterministic = rssm.recurrent(posterior, actions, deterministic)
+    dist, posterior = rssm.transition(deterministic)
+    d_obs = decoder(posterior, deterministic)
+    d_obs = d_obs.mean.squeeze().detach().numpy()
+    obs = ((d_obs.transpose([1,2,0])  + 0.5) * 255).clip(0, 255).astype(np.uint8)
+    img = Image.fromarray(obs, "RGB")
+    frames.append(img)
+
+print("saving gif")    
+frame_one = frames[0]
+frame_one.save(run_path + "/imagine.gif", format="GIF", append_images=frames, save_all=True, duration=30, loop=0)
+print("finished")
--- utils/wrappers.py
+++ utils/wrappers.py
@@ -14,6 +14,28 @@ from dm_control.suite.wrappers import pixels
 import numpy as np
 import cv2
 import os
+from dm_control import suite
+from dm_control.rl.control import Environment
+
+
+class ActionRepeat:
+  def __init__(self, env, amount):
+    self._env = env
+    self._amount = amount
+
+  def __getattr__(self, name):
+    return getattr(self._env, name)
+
+  def step(self, action):
+    done = False
+    total_reward = 0
+    current_step = 0
+    while current_step < self._amount and not done:
+      obs, reward, termination, truncation, info = self._env.step(action)
+      total_reward += reward
+      current_step += 1
+    return obs, total_reward, termination, truncation, info
+
 
 class DMCtoGymWrapper(gym.Env):
     """
